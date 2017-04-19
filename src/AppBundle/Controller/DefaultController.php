2e3af4a22d3f4ed9fecdfdee66465b5481e4d659
Stephen <Blaney.stephen9@gmail.com>
2017-04-19 12:56:40 +0100

    Created the base template for all twig files

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,11 +1,8 @@
 <?php
-
 namespace AppBundle\Controller;
-
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-
 class DefaultController extends Controller
 {
     /**
@@ -13,9 +10,10 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        $argsArray =  [
+            'name' => 'stephen'
+        ];
+        $templateName = 'index';
+        return $this->render($templateName . '.html.twig', $argsArray);
     }
 }
