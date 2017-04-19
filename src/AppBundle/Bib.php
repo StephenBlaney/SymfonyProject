e70045e7f6242660f5157bf877460b5034f78a1e
Stephen <Blaney.stephen9@gmail.com>
2017-04-19 14:11:45 +0100

    Created Bib.php for DIY stroage

--- src/AppBundle/Bib.php
+++ src/AppBundle/Bib.php
@@ -12,4 +12,19 @@ namespace AppBundle;
 class Bib
 {
 
+    private $id;
+    private $name;
+    public function __construct($id, $name){
+        $this->id = $id;
+        $this->name = $name;
+    }
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
+
 }
