eca84a683f6ab51fa50005a96c7e7f776a5ad893
Stephen <Blaney.stephen9@gmail.com>
2017-04-19 14:13:02 +0100

    Created BibRespository.php for DIY stroage

--- src/AppBundle/BibRepository.php
+++ src/AppBundle/BibRepository.php
@@ -6,10 +6,24 @@
  * Time: 13:15
  */
 
-namespace AppBundle\Controller;
+namespace AppBundle;
 
 
 class BibRepository
 {
+    private $bibs = [];
 
+    public function __construct()
+    {
+        $s1 = new Bib(1, 'matt');
+        $s2 = new Bib(2, 'joelle');
+        $s3 = new Bib(3, 'jim');
+        $this->bibs[] = $s1;
+        $this->bibs[] = $s2;
+        $this->bibs[] = $s3;
+    }
+    public function getAll()
+    {
+        return $this->bibs;
+    }
 }
