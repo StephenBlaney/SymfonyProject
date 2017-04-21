61a404990fdffa3e82b6bba1a95ba0d639b56aa4
Stephen <Blaney.stephen9@gmail.com>
2017-04-21 16:12:13 +0100

    Created colour session

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,6 +3,9 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+
 class DefaultController extends Controller
 {
     /**
@@ -10,6 +13,16 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        // create colours array
+        $colours = [
+            'foreground' => 'blue',
+            'background' => 'pink'
+        ];
+        
+        // store colours in session 'colours'
+        $session = new Session();
+        $session->set('colours', $colours);
+
         $argsArray =  [
             'name' => 'stephen'
         ];
